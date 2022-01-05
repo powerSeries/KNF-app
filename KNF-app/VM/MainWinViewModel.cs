1f08ef24f167a4408a5bc9b91c7e9fbafc925179
Jose Sotolongo <joseesotolongo@gmail.com>
2022-01-04 21:49:18 -0500

    Added MainWin ViewModel

---
+++ KNF-app/VM/MainWinViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNF_app.VM
+{
+    public class MainWinViewModel
+    {
+        public string MaxFret { get; set; }
+
+
+        public MainWinViewModel()
+        { 
+
+        }
+    }
+}
